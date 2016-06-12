e9e69d935d33cb02272987425358f326275a969a
bitsandsalsa <bitsandsalsa@users.noreply.github.com>
2016-06-11 22:47:47 -0700

    add arg parser to driver script

--- shelf/runserver.py
+++ shelf/runserver.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python
+import argparse
+
 from shelf import app
-app.run()
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-s', '--host', default='10.0.3.1', help='host address to server from')
+parser.add_argument('-p', '--port', default=8000, type=int, help='TCP port to serve from')
+args = parser.parse_args()
+app.run(args.host, args.port)
